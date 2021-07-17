4b39250612ad965256743110abea0b03c94c54d2
herfalerf <jpendy@gmail.com>
2021-07-17 16:02:26 -0500

    exercise completed, all tests passsing

--- create-account.js
+++ create-account.js
@@ -1,5 +1,34 @@
 function createAccount(pin, amount) {
-
+  let balance = amount || 0;
+  let account = {
+    checkBalance: function (inputPin) {
+      return inputPin === pin ? `$${balance}` : "Invalid PIN.";
+    },
+    deposit: function (inputPin, amount) {
+      if (inputPin !== pin) return "Invalid PIN.";
+      else {
+        balance = balance + amount;
+        return `Succesfully deposited $${amount}. Current balance: $${balance}.`;
+      }
+    },
+    withdraw: function (inputPin, amount) {
+      if (inputPin !== pin) return "Invalid PIN.";
+      else {
+        if (amount > balance)
+          return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+        balance = balance - amount;
+        return `Succesfully withdrew $${amount}. Current balance: $${balance}.`;
+      }
+    },
+    changePin(inputPin, newPin) {
+      if (inputPin !== pin) return "Invalid PIN.";
+      else {
+        pin = newPin;
+        return "PIN successfully changed!";
+      }
+    },
+  };
+  return account;
 }
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
-
+  let number = Math.floor(Math.random() * 100);
+  let numGuess = 0;
+  let gameOver = false;
+  return function game(guess) {
+    if (gameOver) return "The game is over, you already won!";
+    numGuess++;
+    if (guess === number) {
+      gameOver = true;
+      return `You win! You found ${number} in ${numGuess} guesses.`;
+    } else if (guess > number) {
+      return `${guess} is too high!`;
+    } else {
+      return `${guess} is too low!`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
